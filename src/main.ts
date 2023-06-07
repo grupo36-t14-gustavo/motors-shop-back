90e003092654c669d0b3533f1426436fdc796ff9
gabriel <gabrielcoloradogvda@gmail.com>
2023-06-07 03:10:23 -0300

    feat: list car by user id and delete car by id

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import express, { Application } from "express";
 import "express-async-errors";
 import { handlreErrors } from "./errors";
 import userRouter from "./routes/index";
+import adRoutes from "./routes/adRoutes";
 
 export const app: Application = express();
 app.use(express.json());
 
 app.use("", userRouter);
+app.use("/cars", adRoutes);
 
 
 
