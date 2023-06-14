8161f3d914fb8efeb9c262c713383f1b2acc8129
Bryan <bryanbatista79688@gmail.com>
2023-06-14 16:44:55 -0300

    feat: carAdUpdate service and controller

---
+++ src/controllers/CarAds/updateCarAd.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import { updateCarAdService } from "../../services/CarAds/updateCarAd.service";
+import { statusSuccess } from "../../constants";
+
+export const updateCarAdController = async (req: Request, res: Response) => {
+    const carId = req.params.carId;
+
+    const updatedCarAd = await updateCarAdService(req, carId);
+
+    return res.status(statusSuccess.OK).json(updatedCarAd);
+};
