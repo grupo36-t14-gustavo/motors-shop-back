cd8f56e2b1cc11f2ccc446cea30c9c43e52577fb
Bryan <bryanbatista79688@gmail.com>
2023-06-15 17:30:59 -0300

    feat: Initial deploy config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import express, { Application } from "express";
 import "express-async-errors";
+import cors from "cors";
+import express, { Application } from "express";
 import { errorHandlerUtil } from "../src/utils/errorHandler.util";
 import adRoutes from "./routes/CarAds/carAds.routes";
 import userRouter from "./routes/User/user.routes";
 
 export const app: Application = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRouter);
