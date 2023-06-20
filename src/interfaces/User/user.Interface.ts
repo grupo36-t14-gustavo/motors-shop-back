d825e129166b54d0e7bab1c16900789e0e999a40
Denis <denisguedesmoraes@gmail.com>
2023-06-20 18:43:47 -0300

    remove any typings

--- src/interfaces/User/user.Interface.ts
+++ src/interfaces/User/user.Interface.ts
@@ -1,9 +1,10 @@
 import { z } from "zod";
 import { createdAdress } from "../../schemas/User/userAddress.schema";
 import { createdLoginSchema } from "../../schemas/User/userLogin.schema";
-import { createdUserSchema, returnCreatedUserWithPassword } from "../../schemas/User/userRegister.schema";
+import { createdUserSchema, registerUserAndAddress, returnCreatedUserWithPassword } from "../../schemas/User/userRegister.schema";
 
 export type ToffPassword = z.infer<typeof returnCreatedUserWithPassword>;
 export type Tlogin = z.infer<typeof createdLoginSchema>;
 export type TReturnAddress = z.infer<typeof createdAdress>;
 export type TuserRegister = z.infer<typeof createdUserSchema>
+export type TreturnRegister = z.infer<typeof registerUserAndAddress>
--- src/services/User/registerUser.service.ts
+++ src/services/User/registerUser.service.ts
@@ -4,9 +4,7 @@ import { returnCreatedUser } from "../../schemas/User/userRegister.schema";
 
 const prisma = new PrismaClient();
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const createUserService:any = async (userData: TuserRegister, addressData:TReturnAddress ) => {
-    let createdUser;
+export const createUserService = async (userData: TuserRegister, addressData:TReturnAddress ) => {
 
     const newUser = await prisma.user.create({
         data: userData,
@@ -14,21 +12,19 @@ export const createUserService:any = async (userData: TuserRegister, addressData
 
     const parsedUser = returnCreatedUser.parse(newUser);
     
-    // eslint-disable-next-line prefer-const
-    createdUser = parsedUser;
+    const createdUser = parsedUser;
     
     const newAddress = await prisma.address.create({
         data: {
             ...addressData,
             userId: createdUser.id,
-                
+
         },
     });
     const returnData = {
         ...newUser,
         address: newAddress,
     };
-    console.log("return data",returnData);
         
     return returnData;
   
